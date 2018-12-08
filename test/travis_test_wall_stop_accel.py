65828915feb8df1f97e6897a393ec26e445c3a8e
Teable <blukey842unluck@gmail.com>
2018-12-08 10:48:23 +0000

    Fix an error on 'travis_wall_stop_accel.py'

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python
 import unittest, rostest
 import rosnode, rospy
 import time
 
 class WallStopAccelTest(unittest.TestCase):
-	def set_and_get(self,lf,ls.rs.rf):
+	def set_and_get(self,lf,ls,rs,rf):
 		with open("/dev/rtlightsensor0","w") as f:
 			f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
